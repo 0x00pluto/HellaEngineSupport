a6682c569fb0ec48808a00bf0319a73ffd658a17
peng.zhi <babety@163.com>
2016-10-29 12:13:06 +0800

    增加下载函数 download

--- tests/hellaEngine/support/Http/HttpTest.php
+++ tests/hellaEngine/support/Http/HttpTest.php
@@ -1,23 +1,35 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: zhipeng
- * Date: 16/9/8
- * Time: 上午9:32
- */
+    /**
+     * Created by PhpStorm.
+     * User: zhipeng
+     * Date: 16/9/8
+     * Time: 上午9:32
+     */
 
-namespace hellaEngine\support\Http;
+    namespace hellaEngine\support\Http;
 
 
-class HttpTest extends \PHPUnit_Framework_TestCase
-{
-
-    public function testHttp()
+    class HttpTest extends \PHPUnit_Framework_TestCase
     {
-        $httpInstance = new Http();
-        $response = $httpInstance->http("www.baidu.com", Http::METHOD_GET);
 
-        self::assertTrue($response->isSucc());
+        public function testHttp()
+        {
+            $httpInstance = new Http();
+            $response = $httpInstance->http("www.baidu.com", Http::METHOD_GET);
+
+            self::assertTrue($response->isSucc());
+
+        }
+
+        public function testDownload()
+        {
+            $httpInstance = new Http();
+            $response = $httpInstance->download('http://apps.chinapay.com/cpedusetliq/mer/recon/20161020/808080301001052_20161020_20161022050027.txt',
+                '1.txt');
+            self::assertTrue($response->isSucc());
+            unlink('1.txt');
+
+        }
+
 
     }
-}
