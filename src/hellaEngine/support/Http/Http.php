98da30a51bd6ab1df25571e29a02c09e5a11abb7
peng.zhi <zhipeng@66plat.com>
2018-08-11 10:48:37 +0800

    修改HTTP 支持并发请求

--- src/hellaEngine/support/Http/Http.php
+++ src/hellaEngine/support/Http/Http.php
@@ -33,6 +33,72 @@ class Http
      */
     function http($url, $query = "", $method = self::METHOD_GET, $appendCURLOption = [])
     {
+        $ch = $this->create_curl_handle($url, $query, $method, $appendCURLOption);
+        $response = trim(curl_exec($ch));
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $originUrl = trim(curl_getinfo($ch, CURLINFO_EFFECTIVE_URL));
+        curl_close($ch);
+
+        return HttpResponse::create($http_code, $response, $originUrl);
+    }
+
+    /**
+     * 并发请求
+     * @param \Closure|array $curls 通过调用 create_curl_handle生产的CURL句柄
+     * @return HttpResponse []
+     */
+    public function httpMulti($curls)
+    {
+
+        $curls = value($curls);
+        if (empty($curls)) {
+            return [];
+        }
+
+        $mh = curl_multi_init();
+        //加入句柄
+        foreach ($curls as $curl) {
+            curl_multi_add_handle($mh, $curl);
+        }
+
+        //等待完成
+        do {
+            curl_multi_exec($mh, $active);
+        } while ($active);
+
+
+        //读取资源
+        $responses = [];
+        foreach ($curls as $i => $curl) {
+
+            $response = trim(curl_multi_getcontent($curl));
+            $http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+            $originUrl = trim(curl_getinfo($curl, CURLINFO_EFFECTIVE_URL));
+            //获取每一个线程的执行结果
+            $responses[$i] = HttpResponse::create($http_code, $response, $originUrl);
+        }
+
+        foreach ($curls as $i => $curl) {
+            curl_multi_remove_handle($mh, $curl);
+            curl_close($curl);
+        }
+
+        curl_multi_close($mh);
+
+        return $responses;
+    }
+
+    /**
+     * 创建 CURL 句柄
+     * @param $url
+     * @param string $query
+     * @param string $method
+     * @param array $appendCURLOption
+     * @return resource
+     */
+    public function create_curl_handle($url, $query = "", $method = self::METHOD_GET, $appendCURLOption = [])
+    {
         $ch = curl_init();
         switch (strtoupper($method)) {
             case self::METHOD_GET :
@@ -57,14 +123,14 @@ class Http
         foreach ($appendCURLOption as $key => $value) {
             curl_setopt($ch, $key, $value);
         }
-
-        $response = trim(curl_exec($ch));
-        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-
-        return HttpResponse::create($http_code, $response, $url);
+        return $ch;
     }
 
+    /**
+     * 解析GET参数
+     * @param $query
+     * @return string
+     */
     public function parseQueryArray($query)
     {
         if (is_array($query)) {
